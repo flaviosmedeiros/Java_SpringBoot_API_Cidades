1786b03c1bd194ba6ecb6cad88f23733d546a835
Flavio Medeiros <flaviotibra@gmail.com>
2021-05-16 02:43:07 -0300

    first commit

---
+++ src/test/java/br/com/fsmedeiros/apicidades/ApiCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fsmedeiros.apicidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
